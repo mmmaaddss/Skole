bc7bfca86dcebcc3222f2e3861b23709064c9039
mmmaaddss <52237511+mmmaaddss@users.noreply.github.com>
2023-02-05 20:36:02 +0100

    New fizzbuzz version

---
+++ fizzbuzzupgrade.py
@@ -0,0 +1,21 @@
+print('Hvad skal jeg tælle til?') #guider useren
+userrange = input() #tager imod et input
+
+def isDisibleBy3(): #functioner, som gør koden mere læsbar
+    if i % 3 == 0:
+        print('fizz')
+def isDisibleBy5():
+    if i % 5 == 0:
+        print('buzz')
+def isDisibleBy3and5():
+    if i % 3 == 0 and i % 5 == 0:
+        print('fizzbuzz')
+
+for i in range(int(userrange)):
+    if isDisibleBy3and5(): #kalder function
+        continue
+    elif isDisibleBy3():
+        continue
+    elif isDisibleBy5():
+        continue
+    print(i)
