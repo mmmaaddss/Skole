3e0ba2ac38075346bbbca37b85b93b202c67a0c1
mmmaaddss <madsbaunb@gmail.com>
2023-02-22 11:48:34 +0100

    inputtet skal være et tal nu

--- fizzbuzzupgrade.py
+++ fizzbuzzupgrade.py
@@ -1,12 +1,20 @@
-print('Hvad skal jeg tælle til?') #guider useren
-userrange = input() #tager imod et input
+
+def giverAltidEnFloat(askThisQuestion):
+    tastet = input(askThisQuestion)
+    try:
+        return float(tastet)
+    except ValueError:
+        print(f"Du tastede {tastet}... Det lykkedes ikke at tolke som et tal. Prøv igen...")
+        return giverAltidEnFloat(askThisQuestion) #https://media.tenor.com/C_typNW6OgEAAAAC/recursion-winnie.gif
+
+userrange = giverAltidEnFloat("Hvad skal jeg tælle til?")
 
 def isDisibleBy3(): #functioner, som gør koden mere læsbar
     if i % 3 == 0:
         print('fizz')
 def isDisibleBy5():
     if i % 5 == 0:
-        print('buzz') #buzzin
+        print('buzz')
 def isDisibleBy3and5():
     if i % 3 == 0 and i % 5 == 0:
         print('fizzbuzz')
